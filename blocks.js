d6047ccf1efe044a0f211843d7f40202a3c786b7
wwmairs <wwmairs@gmail.com>
2017-12-09 18:12:53 -0500

    Blocks instance contains many categories of Block

--- blocks.js
+++ blocks.js
@@ -25,44 +25,83 @@ const B_RATIO   = B_SIZE + B_PADDING
 // an instance of a Blocks class should make:
 //      - a wrapper for all of the Blocks it contains
 //      - a square for each Block it contains
+
+// Blocks contructor is passed a list of objects that look like this:
+// {"name"  : "categoryName",
+//  "size"  :  categorySize,
+//  "color" : "someColor"}
 class Blocks {
-    constructor(_x, _y, width, height, num_blocks) {
-        this.x  = _x;
-        this.y  = _y;
-        this.w  = width;
-        this.h  = height;
-        this.bs = [];
-        this.g  = document.createElementNS(svgns, "g");
+    constructor(_x, _y, width, height, categoryInfo) {
+        this.x    = _x;
+        this.y    = _y;
+        this.w    = width;
+        this.h    = height;
+        this.cats = [];
+        this.g    = document.createElementNS(svgns, "g");
         this.g.setAttribute("class", "block-chart");
         svg.appendChild(this.g);
-        // make an array of Block s
-        let blocks_wide = Math.ceil(width / B_RATIO);
-        let blocks_tall = Math.ceil(height / B_RATIO);
-        this.totalBlocks = blocks_wide * blocks_tall;
-        console.log(this.totalBlocks);
-        for (let i = 0; i < blocks_wide; i++) {
-            for (let j = 0; j < blocks_tall; j++) {
-                this.bs[i] = new Block(this.g, this.x + i * B_RATIO, this.y + j * B_RATIO, "black")
+
+        for (let n = 0; n < categoryInfo.length; n++) {
+            let info = categoryInfo[n];
+            info["blocks"] = [];
+            this.cats.push(info);
+        }
+        var sum = 0;
+        for (let n = 0; n < this.cats.length; n++) {
+            sum += cats[n].size;
+        }
+        this.totalSize = sum;
+
+        var blocks_wide = Math.ceil(width / B_RATIO);
+        var blocks_tall = Math.ceil(height / B_RATIO);
+        this.capacity = blocks_wide * blocks_tall;
+        console.assert(this.totalSize < this.capacity);
+        var i = 0;
+        var j = 0;
+        var counter = 0;
+        // loop through cats drawing each category
+        for (let n = 0; n < this.cats.length; n++) {
+            let c = this.cats[n];
+            counter = 0;
+            while (counter < c.size) {
+                c.blocks[counter] = new Block(this.g, this.x + i * B_RATIO, this.y + j * B_RATIO, c.color, c.name);
+                counter++;
+                j++;
+                if (j == blocks_tall) {
+                    i++;
+                    j = 0;
+                }
+                if (i == blocks_wide) {
+                    console.error("ran out of room trying to draw blocks for category " + c.name + " in Block " + this);
+                    // we should never get here if the assert works
+                }
             }
         }
+    }
+
+    getCategory(name) {
+        // returns category from this.cats where name == cats[_].name;
     }
 }
 
 class Block {
-    constructor(container, _x, _y, _c) {
+    constructor(container, _x, _y, _c, _name) {
         this.parent = container;
         this.x = _x;
         this.y = _y;
         this.c = _c;
+        this.category = _name;
         this.on = true;
         this.b = document.createElementNS(svgns, "rect");
         this.b.setAttribute("x", this.x);
         this.b.setAttribute("y", this.y);
         this.b.setAttribute("width", B_SIZE);
         this.b.setAttribute("height", B_SIZE);
+        this.b.setAttribute("fill", this.c);
         this.parent.appendChild(this.b);
+        let parent = this;
         this.b.addEventListener("click", function(event) {
-            toggle();
+            console.log(parent.category);
         });
     }
 
@@ -74,10 +113,25 @@ class Block {
 
 }
 
+// Blocks contructor is passed a list of objects that look like this:
+// {"name"  : "categoryName",
+//  "size"  :  categorySize,
+//  "color" : "someColor"}
+
 // here's where it all begins
 let container = document.getElementById("container");
 let svg = document.createElementNS(svgns, "svg");
 svg.setAttribute("width", window.innerWidth);
 svg.setAttribute("height", 500);
 container.appendChild(svg);
-let b = new Blocks(10, 10, 600, 400, 40);
+let sampleCat1 = {"name" : "murder",
+                  "size" : 500,
+                  "color" : "black"};
+let sampleCat2 = {"name" : "suicide",
+                  "size" : 300,
+                  "color" : "blue"};
+let sampleCat3 = {"name" : "accident",
+                  "size" : 100,
+                  "color" : "red"};
+let cats = [sampleCat1, sampleCat2, sampleCat3];
+let b = new Blocks(10, 10, 600, 400, cats);
