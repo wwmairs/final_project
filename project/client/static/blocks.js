6773e090d84e521a1e119726a5726d06c132844d
wwmairs <wwmairs@gmail.com>
2017-12-11 17:13:35 -0500

    merging blocks.js

--- project/client/static/blocks.js
+++ project/client/static/blocks.js
@@ -41,7 +41,7 @@ class Canvas {
 }
 
 class Blocks {
-    constructor(_x, _y, width, height, num_blocks) {
+    constructor(_x, _y, width, height, numBlocks) {
         this.x  = _x;
         this.y  = _y;
         this.w  = width;
@@ -51,16 +51,48 @@ class Blocks {
         this.g.setAttribute("class", "block-chart");
         svg.appendChild(this.g);
         // make an array of Block s
-        let blocks_wide = Math.ceil(width / B_RATIO);
-        let blocks_tall = Math.ceil(height / B_RATIO);
-        this.totalBlocks = blocks_wide * blocks_tall;
-        console.log(this.totalBlocks);
-        for (let i = 0; i < blocks_wide; i++) {
-            for (let j = 0; j < blocks_tall; j++) {
-                this.bs[i] = new Block(this.g, this.x + i * B_RATIO, this.y + j * B_RATIO, "black")
+        this.blocksWide = Math.ceil(width / B_RATIO);
+        this.blocksTall = Math.ceil(height / B_RATIO);
+        this.capacity = this.blocksWide * this.blocksTall;
+        let counter = 0
+        for (let i = 0; i < this.blocksWide; i++) {
+            for (let j = 0; j < this.blocksTall; j++) {
+                this.bs[counter] = new Block(this.g, this.x + i * B_RATIO, this.y + j * B_RATIO, "black");
+                counter++;
             }
         }
+        this.makeSquare(numBlocks);
     }
+
+    makeSquare(numBlocks) {
+        this.allOff();
+        let side = Math.floor(Math.sqrt(numBlocks));
+        console.log(side);
+        let count = 0;
+        for (var x = 0; x < side; x++) {
+            for (var y = 0; y < side; y++) {
+                this.bs[x + this.blocksTall * y].turnOn();
+                count++;
+            }
+        }
+        // this part is supposed to fill in the extra parts on one side
+        // it doesn't work yet
+        while (count < numBlocks) {
+            y =
+            this.bs[x + this.blocksTall * y].turnOn();
+            y++;
+            count++;
+        }
+    }
+
+    allOff() {
+        for (let n = 0; n < this.bs.length; n++) {
+            this.bs[n].turnOff();
+        }
+    }
+
+
+
 }
 
 class Block {
@@ -76,14 +108,29 @@ class Block {
         this.b.setAttribute("width", B_SIZE);
         this.b.setAttribute("height", B_SIZE);
         this.parent.appendChild(this.b);
+        let parent = this;
         this.b.addEventListener("click", function(event) {
-            toggle();
+            parent.toggle();
         });
+    }
+
+    turnOff() {
+        this.on = false;
+        this.b.setAttribute("fill", "white");
+    }
+    turnOn() {
+        this.on = true;
+        this.b.setAttribute("fill", this.c);
     }
 
     toggle() {
         this.on = ! this.on;
         this.on ? this.b.setAttribute("fill", this.c) : this.b.setAttribute("fill", "white");
+    }
+
+    setColor(_c) {
+        this.c = _c;
+        this.b.setAttribute("fill", this.c);
     }
 
 
@@ -132,5 +179,5 @@ let svg = document.createElementNS(svgns, "svg");
 svg.setAttribute("width", window.innerWidth);
 svg.setAttribute("height", 800);
 container.appendChild(svg);
-let b = new Blocks(10, 10, 600, 400, 40);
+let b = new Blocks(10, 10, 600, 400, 400);
 svg.appendChild(newText)
